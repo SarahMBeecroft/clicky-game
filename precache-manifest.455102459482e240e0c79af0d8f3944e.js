589b10bf2c5c6011fd53f1fb043519f771e35c12
Sarah B <smblair87@gmail.com>
2019-07-10 18:33:39 -0700

    Updates

---
+++ precache-manifest.455102459482e240e0c79af0d8f3944e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab8ce9de23420823c64e081f89f333a",
+    "url": "/SarahMBeecroft/clicky-game/index.html"
+  },
+  {
+    "revision": "3ad1e93397826c4c0c37",
+    "url": "/SarahMBeecroft/clicky-game/static/css/main.6905ce2a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/SarahMBeecroft/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3ad1e93397826c4c0c37",
+    "url": "/SarahMBeecroft/clicky-game/static/js/main.dfcb38b2.chunk.js"
+  },
+  {
+    "revision": "713687e9ac7b6fbdd1fc",
+    "url": "/SarahMBeecroft/clicky-game/static/js/runtime~main.6f8038ef.js"
+  }
+]);
